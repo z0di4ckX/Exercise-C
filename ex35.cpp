f79f8876ed043a32367399825bb5d0e7faf540d2
Wesly <ultimatecracker88@gmail.com>
2019-08-28 00:14:37 -0400

    fix Exercise 35 more for matriz

--- ex35.cpp
+++ ex35.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main() {
 	int numero[100][100], filas, columnas;
+	char band = 'F';
 
 	cout << "Escriba el numero de filas: ";
 	cin >> filas;
@@ -13,10 +14,31 @@ int main() {
 	cin >> columnas;
 
 	for(int i = 0; i < filas; i++) {
-		for(int j = 0; i < columnas; j++) {
-			
+		for(int j = 0; j < columnas; j++) {
+			cout << "Escriba un numero [" << i << "][" << j << "]: ";
+			cin >> numero[i][j];
+		}
+		cout << "\n";
+	}
+
+	if(filas == columnas) {
+		for(int i = 0; i < filas; i++) {
+			for(int j = 0; j < columnas; j++) {
+				if(numero[i][j] == numero[j][i]) {
+					band = 'V';
+				}
+			}
+			cout << "\n";
 		}
 	}
 
+	if(band == 'V') {
+		cout << "La matriz es simetrica" << endl;
+	}
+	else {
+		cout << "La matriz no es siemtrica" << endl;
+	}
+
+	getch();
 	return 0;
 }
