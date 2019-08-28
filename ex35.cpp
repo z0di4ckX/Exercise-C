1fb3535d65fad6084f700d3518736b9f2cfc2472
Wesly <ultimatecracker88@gmail.com>
2019-08-27 23:58:45 -0400

    Exercise 35

---
+++ ex35.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int numero[100][100], filas, columnas;
+
+	cout << "Escriba el numero de filas: ";
+	cin >> filas;
+
+	cout << "Escriba el numero de columnas: ";
+	cin >> columnas;
+
+	for(int i = 0; i < filas; i++) {
+		for(int j = 0; i < columnas; j++) {
+			
+		}
+	}
+
+	return 0;
+}
