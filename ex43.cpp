0589dd84036d39210e6c668d7096a244b3f25c44
Wesly <ultimatecracker88@gmail.com>
2019-09-13 19:46:04 -0400

    Exercise 42 strlwr function

---
+++ ex43.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    char cad[] = "JUAN";
+
+    strlwr(cad); // inverdir el nombre Pablo
+
+    cout << cad << endl;
+    getch();
+    return 0;
+}
