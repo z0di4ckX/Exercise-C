7b0c6145e093ade2f021e4d53f423b0a3816e7e3
Wesly <ultimatecracker88@gmail.com>
2019-09-14 19:32:39 -0400

    Exercise 43 strlwr function add one more function

--- ex43.cpp
+++ ex43.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 int main() {
     char cad[] = "JUAN";
+    char cad1[] = "ALEJGANDRO";
 
     strlwr(cad); // convierte a minuscula el nombre juan
+    strlwr(cad1);
 
-    cout << cad << endl;
+    cout << cad << cad1 << endl;
     getch();
     return 0;
 }
